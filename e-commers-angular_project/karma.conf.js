73002a4402e15b910e480924475b9fac5282c6f5
Shivam Solanki <rajputshivam2906@.in>
2024-03-30 12:48:24 +0530

    setup-project

--- e-commers-angular_project/karma.conf.js
+++ e-commers-angular_project/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/e-commers_project'),
+      dir: require('path').join(__dirname, './coverage/e-commers-angular_project'),
       subdir: '.',
       reporters: [
         { type: 'html' },
